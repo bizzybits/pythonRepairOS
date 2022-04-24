15f2b2b195dc5cb989200d65735dd9e391078825
Elizabeth Ponce <ponce.emc@gmail.com>
2022-04-23 17:34:22 -0700

    fixed documentation loop

--- main.py
+++ main.py
@@ -42,9 +42,53 @@ def print_repair_ticket(item):
     print(f"{item.item_name} repair is due on {item.date}")
 
 
+def faqs():
+    print('''
+                            ** Frequently Asked Questions **
+                            
+                What kind of items can I enter?
+                    
+                    Any! You decide, the program is designed to be flexible
+                    for your needs.
+                
+                What kind of payments does the software accept?
+                    
+                    This is up to you -- the payments processing module
+                    will be updated to function in an upcoming software
+                    update release.
+                    
+                What if everything freezes?
+                
+                    Turn it off then turn it off again.
+                    
+                More questions will be added as features roll out.
+                    
+          ''')
+
+
+def documentation():
+    print('''
+                            ** General Documentation **
+                RepairOS is a helpful software program that allows you to 
+                manage your repair shop's intake and payment processes.
+                
+                When you select 1 at the main menu, you will be prompted
+                through a series of entries to input information about the
+                item to repair.
+                
+                Each step of the way you will receive instructions on next 
+                steps.
+                
+                Finally you will be asked if the information entered is 
+                correct -- if not you will return to the main menu to restart.
+          ''')
+
+
 def is_correct():
     print("Is this information correct?\n")
-    validate = input("Enter 'y' to confirm or 'n' to go back to main menu: ")
+    validate = input(
+        "Enter 'y' to confirm and print Repair Ticket\nor 'n' to go back to main menu: "
+    )
     return validate
 
 
@@ -66,11 +110,6 @@ def create(Item=None):
     repair_item.show_full_item()
 
 
-# def run_the_counts(count, name):
-#     for _ in range(count):
-#         click.echo(f"Hello {name}")
-
-
 def help():
     print(f"""
                **************
@@ -79,10 +118,10 @@ def help():
                1- Documentation
                2- FAQs
                
-               Exit
+               Return to Main Menu
                **************
                """)
-    article = input("Enter a number or type 'Exit' to exit: ")
+    article = input("Enter a number or type 'Main' to Return to Main Menu: ")
     return article
 
 
@@ -104,11 +143,11 @@ def menu():
                1- Enter a New Repair Item
                2- Help
                
-               Exit
+               Exit RepairOS
                **************
                
                """)
-    direction = input("Enter a number or type 'Exit' to exit: ")
+    direction = input("Enter a number or type 'Exit' to exit RepairOS: ")
     return direction
 
 
@@ -137,37 +176,51 @@ def justatest(Item=None):
 
 def main():
     greeting()
-    direction = menu()
-    print("\n")
-    while direction == "1":
-
-        item = Item.get_new_repair()
-        item.show_full_item()
-        print("\n")
-        validate = is_correct()
-        print("\n")
-        if validate == "y":
-            print_repair_ticket(item)
-            direction = menu()
-
-        if validate == "n":
-            direction = menu()
-
-    if direction == "2":
-        article = help()
-        if article == "1":
-            print("Documentation")
-            direction = menu()
-        if article == "2":
-            print("FAQs")
-            direction = menu()
-
-    if direction == "Exit" or "exit":
-        print_repair_ticket(item)
-        print("Thank you, have a great day!")
-    else:
-        print("invalid command, try again")
+    while 1:
         direction = menu()
+        print("\n")
+        if direction == "1":
+
+            item = Item.get_new_repair()
+            item.show_full_item()
+            print("\n")
+            validate = is_correct()
+            print("\n")
+            if validate == "y":
+                print_repair_ticket(item)
+                direction = menu()
+
+            if validate == "n":
+                direction = menu()
+            else:
+                if direction.upper() == "EXIT":
+                    print("Thank you, have a great day!")
+                    break
+
+        if direction == "2":
+            article = help()
+            if article == "1":
+                documentation()
+                article = help()
+            if article == "2":
+                faqs()
+                article = help()
+            if article.upper() == "MAIN":
+                direction = menu()
+            else:
+                print("invalid command, try again")
+                direction = menu()
+                if direction.upper() == "EXIT":
+                    print("Thank you, have a great day!")
+                    break
+
+        if direction.upper() == "EXIT":
+            print("Thank you, have a great day!")
+            break
+
+        # else:
+        #     print("invalid command, try again")
+        #     direction = menu()
 
 
 if __name__ == '__main__':
